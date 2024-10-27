cfa5b72a06ccfbc699f45cf40e277d4c1d03a8bf
rain-1107 <ryan.bays@scchs.org.uk>
2024-10-27 10:35:15 +0000

    added ufo

--- after/plugin/conform.lua
+++ after/plugin/conform.lua
@@ -2,7 +2,7 @@ require("conform").setup({
     formatters_by_ft = {
         cpp = { lsp_format = "clangd" },
         lua = { lsp_format = "lua_ls" },
-        python = { lsp_format = "pyright" },
+        python = { lsp_format = "jedi_language_server" },
         rust = { lsp_format = "rust_analyzer" },
         go = { lsp_format = "gopls" },
         javascript = {},
